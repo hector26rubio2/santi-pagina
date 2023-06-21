ded71e8266044f6283820bc58ed556bfc5297df5
hector26rubio2 <hector26rubio@gmail.com>
2023-06-20 19:40:13 -0500

    Update Inicio.jsx

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -73,7 +73,7 @@ export const Inicio = () => {
 					<Card style={{ width: "18rem" }}>
 						<Card.Img variant="top" src="sdsadas.jpg" />
 						<Card.Body>
-							<Card.Title>Card Title</Card.Title>
+							<Card.Title>prueba iamgen</Card.Title>
 							<Card.Text>
 								Some quick example text to build on the card title and make up
 								the bulk of the card's content.
